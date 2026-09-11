1d4555dd855e54e1aed81ab32cabaf499ee2240a
the-deep-33 <petnik033@gmail.com>
2026-09-11 12:44:26 +0200

    Fixed loader.py, only rows with the full 24h data are taken into consideration

--- loader.py
+++ loader.py
@@ -6,13 +6,13 @@ import time
 
 def load_dataset(timeframe):
     RAW_FILE = f'btcusdt_{timeframe}_raw.parquet'
+    interval = ccxt.binance().parse_timeframe(timeframe) * 1000
     if os.path.exists(RAW_FILE):
         df = pd.read_parquet(RAW_FILE)
     else:
         exchange = ccxt.binance()
         symbol = 'BTC/USDT'
         since = exchange.parse8601('2022-01-01T00:00:00Z')
-        interval = exchange.parse_timeframe(timeframe) * 1000
 
         all_rows = []
         while True:
@@ -22,6 +22,9 @@ def load_dataset(timeframe):
             all_rows += batch
             since = batch[-1][0] + interval
             time.sleep(exchange.rateLimit / 1000)
+
+        now_ms = exchange.milliseconds()
+        all_rows = [row for row in all_rows if row[0] + interval <= now_ms]
 
         df = pd.DataFrame(all_rows, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
         df['date'] = pd.to_datetime(df['timestamp'], unit='ms')
@@ -33,5 +36,9 @@ def load_dataset(timeframe):
     
     if df.timestamp.duplicated().any() == True:
         raise ValueError("Timestamps are not unique!")
+
+    
+    if df.timestamp.iloc[-1] + interval > time.time() * 1000:
+        raise ValueError("Last candle is still in progress!")
     
     return df
